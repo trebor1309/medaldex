436568e1b47139a01a7773a3937828fab0cb900e
trebor1309 <robert-heinen@hotmail.com>
2025-10-02 04:24:36 +0200

    Update and rename config.ts to config.js

--- config.ts
+++
@@ -1,2 +0,0 @@
-export const SUPABASE_URL = "https://xwdvnjodqvwqvorbnxln.supabase.co";
-export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh3ZHZuam9kcXZ3cXZvcmJueGxuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkzNDkzMzAsImV4cCI6MjA3NDkyNTMzMH0.219yAB1IUJTRbTAR3gt8h9ufkI50-TJqKej-iW9KOmM";
